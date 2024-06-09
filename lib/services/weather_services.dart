b3fbbcf2b0ee9af99d76d0c76beeeaf89bf5b648
shehab358 <shehabyusuf12@gmail.com>
2024-06-09 13:31:20 +0300

    Errors Handled

--- lib/services/weather_services.dart
+++ lib/services/weather_services.dart
@@ -1,3 +1,4 @@
+import 'dart:developer';
 import 'package:dio/dio.dart';
 import 'package:weather_app/models/weather_model.dart';
 
@@ -6,11 +7,18 @@ class WeatherServices {
   final String baseUrl = "http://api.weatherapi.com";
   final String apiKey = '7e74740cad0340cd982183017240706';
   WeatherServices({required this.dio});
-  Future<WeatherModel> getCurrentWeather({required String city}) async {
-    Response response = await dio.get(
-        '$baseUrl/v1/forecast.json?key=$apiKey&q=$city&days=1');
-    WeatherModel weatherModel = WeatherModel.fromJson(response.data);
-
-    return weatherModel;
+  Future<WeatherModel?> getCurrentWeather({required String city}) async {
+    try {
+      Response response =
+          await dio.get('$baseUrl/v1/forecast.json?key=$apiKey&q=$city&days=1');
+      WeatherModel weatherModel = WeatherModel.fromJson(response.data);
+    } on DioException catch (e) {
+      final String errMsg = e.response?.data['error']['message'] ??
+          'Something bad happened, please try again later ';
+      throw Exception(errMsg);
+    } catch (e) {
+      log(e.toString());
+      throw Exception('Something bad happened, please try again later ');
+    }
   }
 }
