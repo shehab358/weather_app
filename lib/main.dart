f3e4b7519f1d16acddafd28392cc4070f7a5d3e9
shehab358 <shehabyusuf12@gmail.com>
2024-06-12 03:06:47 +0300

    Cubit Provided with Bloc

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_current_weather_cubit/get_weather_cubit.dart';
 import 'package:weather_app/view/homeview.dart';
 
 void main() {
   runApp(
-    MaterialApp(
-      home: Homescreen(),
-    ),
+    WeatherApp()
   );
+}
+
+class WeatherApp extends StatelessWidget {
+  const WeatherApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BlocProvider(
+      create: (context) => GetWeatherCubit(),
+      child: MaterialApp(
+          home: Homescreen(),
+        ),
+    );
+  }
 }
