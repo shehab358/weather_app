458cafe4ae3565d28ba3b0ad28c8867c63b416d7
shehab358 <shehabyusuf12@gmail.com>
2024-06-12 03:21:14 +0300

    Cubit Triggered

--- lib/screens/searchscreen.dart
+++ lib/screens/searchscreen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_current_weather_cubit/get_weather_cubit.dart';
 
 class Searchscreen extends StatelessWidget {
   const Searchscreen({super.key});
@@ -34,7 +36,11 @@ class Searchscreen extends StatelessWidget {
               Padding(
                 padding: const EdgeInsets.all(16),
                 child: TextField(
-                  onSubmitted: (value) {} ,
+                  onSubmitted: (value) {
+                    var getWeatherCubit = BlocProvider.of<GetWeatherCubit>(context);
+                    getWeatherCubit.fetchWeather(value: value);
+                    Navigator.pop(context);
+                  } ,
                   decoration: InputDecoration(
                     border: OutlineInputBorder(
                       borderSide: const BorderSide(color: Colors.white),
